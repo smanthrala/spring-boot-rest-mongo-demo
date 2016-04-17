49902a18e3b7fdcd1aaa20735da8d69217ebba27
Sriram_Manthrala <smanthra@gmail.com>
2016-04-17 14:21:02 -0500

    c

--- src/main/java/com/example/person/controller/PersonController.java
+++ src/main/java/com/example/person/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +40,7 @@ public class PersonController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value="/person")
-	public Person savePerson(Person p) throws Exception{
+	public Person savePerson(@RequestBody Person p) throws Exception{
 		
 		try {
 			service.createPerson(p);
